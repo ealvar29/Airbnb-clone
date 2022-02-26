b743aa028d0634046ec4ecd5591adbb4eaffaa9f
ealvar29 <edalvarez2015@gmail.com>
2022-02-26 10:34:04 -0600

    adding header component and configured next.config file for domains

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+
 function Header() {
   return (
     <div>
       <h1>Header component</h1>
+      <div>
+        <Image src="https://links.papareact.com/qd3" layout="fill" />
+      </div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
